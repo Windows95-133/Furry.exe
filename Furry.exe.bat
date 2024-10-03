c3178d0d75a656115ffc66aa487ff0b3dd5f6a89
Windows95-133 <windows95roblox@gmail.com>
2024-10-03 15:18:57 -0800

    Add files via upload

---
+++ Furry.exe.bat
@@ -0,0 +1,19 @@
+{virus open}
+[start Warning]
+[test on screen]
+[okay]
+[start last Warning]
+[Start Virus]
+(start furry.exe]
+(Start color's)
+[red color]
+[green color]
+[blue color]
+[mix color's]
+(screen zoom in 10000000&)
+(screen zoom out 1000%)
+{shape on screen}
+[more shapes]
+[start blue of death]
+[destory computer]
+end
