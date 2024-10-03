5b95a8d570d91f0c5d2bdb24e2aa5110d9aa5069
Windows95-133 <windows95roblox@gmail.com>
2024-10-03 15:25:39 -0800

    Add files via upload

---
+++ oqoe11wwe.exe.bat
@@ -0,0 +1,29 @@
+start oqoe11wwe
+glitch win11
+color's
+2000 color
+1000 color
+shapes
+100 shapes
+more shapes
+destroy this computer
+let more color's on this computer
+Warning for this virus
+[last Warning]
+start oqoe11wwe
+loop
+loop
+loop
+loop
+loop
+loop
+loop
+loop
+loop
+loop
+screen rotate 100%
+screen Zoom out 1000%
+loop
+loop
+goto loop
+end
